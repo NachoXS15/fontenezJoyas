6acffba92b9d8ccad03e0b41557c05d8a6ac6c5e
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-08-17 02:59:16 -0300

    created header and section1 layout

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/def8mld.css%22%3E">
+</head>
+<body>
+    <header>
+        <a href="#">asd</a>
+        <a href="#">asd</a>
+        <a href="#">asd</a>
+        <a href="#">asd</a>
+    </header>
+    <section id="sect1">
+        <div id="sect1-1">
+
+        </div>
+        <div id="sect1-2">
+            <h2>Somos Fonteñez</h2>
+        </div>
+    </section>
+</body>
+</html>
