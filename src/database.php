aa139ac9180d4cf5acb0867134cfca82705624bc
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-09-06 02:00:48 -0300

    created header and 1st section for index, applied styles

--- src/database.php
+++ src/database.php
@@ -1,6 +1,6 @@
 <?php
     try {
-        $conection = new mysqli ("localhost", "root", "", "fontenezJoyas");
+        $conection = new mysqli ("localhost", "root", "", "fontenezjoyas");
     } catch (\Throwable $e) {
         echo "Error en la conexion: " . $e -> getMessage();
     }
