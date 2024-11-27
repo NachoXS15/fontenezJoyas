197c6cbc004b92b7ce25756ebe1dd6bf7cb49e6f
NachoXS-NET <pantojaignacio642@gmail.com>
2024-11-27 13:24:06 -0300

    creating header and routing

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { CloseIcon, MenuIcon } from "../assets/Icons";
+
+export default function Header() {
+    const [isClick, setIsClick] = useState(false);
+    const toggleNavBar = () => {
+        setIsClick(!isClick)
+    }
+    return (
+        <>
+            <div className='bg-indigo-500 flex justify-between items-center py-4 md:py-4 px-7 md:justify-around select-none drop-shadow-sm'>
+                <NavLink to="/">
+                    {/* <img src={logo} width={150} alt="" /> */}
+                    <h2>logo</h2>
+                </NavLink>
+                <nav className='hidden md:flex justify-center items-center gap-10'>
+                    <NavLink className="mx-5 text-lg text-bluemain md:hover:scale-150 transition" style={{ fontFamily: 'Mundial' }} to="/paquetes">Paquetes</NavLink>
+                    <NavLink className="mx-5 text-lg text-bluemain md:hover:scale-150 transition" style={{ fontFamily: 'Mundial' }} to="/about">Nosotros</NavLink>
+                    <form action="">
+                        <input type="text" className="h-7 rounded-full" />
+                    </form>
+                </nav>
+                <div className='md:hidden flex items-center justify-center' onClick={toggleNavBar}>
+                    {isClick ? <CloseIcon /> : <MenuIcon />}
+                </div>
+            </div>
+            {isClick && (
+                <div className=' md:hidden h-fit py-2 bottom-0 shadow-2xl'>
+                    <hr className='w-3/5 m-auto border-2 border-bluemain' />
+                    <div className='flex flex-col mt-3'>
+                        <NavLink className="hover:scale-105 transition my-2 mx-9 text-lg text-bluemain" style={{ fontFamily: 'Mundial' }} to="/paquetes">Paquetes</NavLink>
+                        <NavLink className="hover:scale-105 transition my-2 mx-9 text-lg text-bluemain" style={{ fontFamily: 'Mundial' }} to="/testimonies">Testimonios</NavLink>
+                        <NavLink className="hover:scale-105 transition my-2 mx-9 text-lg text-bluemain" style={{ fontFamily: 'Mundial' }} to="/about">Nosotros</NavLink>
+                        <NavLink className="hover:scale-105 transition my-2 mx-9 text-lg text-bluemain" style={{ fontFamily: 'Mundial' }} to="/contactanos">Contactanos</NavLink>
+                    </div>
+                </div>
+            )}
+        </>
+
+    )
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Index from './pages/Index.tsx'
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Index />
+    },
+    {
+      path: "/products",
+    }
+  ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
