e543f086aed5166bec6e7e906edb010fdf03c144
NachoXS-NET <pantojaignacio642@gmail.com>
2024-12-26 21:34:44 -0300

    created categories files

---
+++ src/config/Categories.ts
@@ -0,0 +1,16 @@
+const CategoriesJoyas = [
+    "Anillos",
+    "Aros",
+    "Pulseras",
+    "Collares"
+]
+
+const CategoriesArt = [
+    "Cubiertos",
+    "Mates"
+]
+
+export {
+    CategoriesArt,
+    CategoriesJoyas
+}
