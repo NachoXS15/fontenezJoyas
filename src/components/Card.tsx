ed1a432dc3e216e3ce4b32de3d3edab7fefefaff
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-12-28 11:13:38 -0300

    updated responsive layout for products

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,13 +3,12 @@ import img from '../assets/anillo.jpg'
 
 export default function Card() {
     return (
-        <div className="w-full md:w-full lg:w-fit bg-slate-200 rounded-lg my-2 py-3 px-2 md:px-4 md:py-9 flex flex-row md:flex-col md:items-start md:justify-between items-center gap-4 md:gap-5 md:hover:scale-105 md:hover:shadow-sm md:hover:cursor-pointer transition">
-            <img src={img} className='w-40 md:w-60 m-auto' alt="" />
+        <div className="w-fit bg-slate-200 rounded-lg m-2 flex px-5 py-10 flex-col items-start justify-between gap-5 hover:scale-105 hover:shadow-sm hover:cursor-pointer transition">
+            <img src={img} className='md:w-60' alt="" />
             <div className="flex flex-col">
-                <h2 className="text-3xl md:text-2xl font-bold text-blue-800">Anillo</h2>
-                <h4 className="text-md text-wrap text-gray-400">anillo todo fachero zarpado nazi</h4>
-                <h3 className="text-2xl text-gray-400">$100000</h3>
-                <NavLink to="/producto" className='bg-blue-800 w-fit mt-5 text-white rounded-full px-5 py-1 hover:bg-white hover:text-blue-800'>Ver más</NavLink>
+                <h2 className="md:text-xl lg:text-2xl font-bold text-blue-800">Anillo</h2>
+                <h4 className="text-md text-gray-400">anillo todo fachero zarpado nazi</h4>
+                <h3 className="text-xl text-gray-400">$100000</h3>
             </div>
         </div>
     )
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -8,7 +8,7 @@ export default function Products() {
         <main className="max-w-full flex justify-center font-serif">
             <Categories />
             <section className="w-full md:w-9/12 flex items-start justify-center m-auto bg-slate-50 px-5 md:px-10 py-12">
-                <div className="w-full flex flex-wrap items-start justify-center gap-4 md:gap-9">
+                <div className="w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-1">
                     <Card />
                     <Card />
                     <Card />
