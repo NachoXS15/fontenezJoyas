1aa82407be80c18ea3ddfde6c9a4067cef466d8e
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2025-01-09 12:13:08 -0300

    updated layout for index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     }
     return (
         <>
-            <header className='bg-purplemain text-white flex justify-between items-center py-6 px-20 md:py-4 md:px-7 md:justify-around select-none drop-shadow-sm' style={{fontFamily: 'quador'}}>
+            <header className='bg-purplemain text-white flex justify-between items-center py-6 px-12 md:py-4 md:px-7 md:justify-around select-none drop-shadow-sm' style={{fontFamily: 'quador'}}>
                 <NavLink to="/">
                     <img src={logo} className="w-14 md:w-10 hover:scale-105 transition" alt="" />
                 </NavLink>
@@ -29,7 +29,7 @@ export default function Header() {
             {isClick && (
                 <div className='md:hidden bg-slate-200 h-fit py-9 bottom-0'>
                     <hr className='w-3/5 m-auto border border-purplemain' />
-                    <div className='flex flex-col mt-3' style={{fontFamily: 'quador'}}>
+                    <div className='flex flex-col mt-3 px-10' style={{fontFamily: 'quador'}}>
                         <NavLink className="hover:scale-105 transition my-2 mx-9 text-2xl text-purplemain" to="/productos">Productos</NavLink>
                         <NavLink className="hover:scale-105 transition my-2 mx-9 text-2xl text-purplemain" to="/about">Nosotros</NavLink>
                     </div>
--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -16,7 +16,7 @@ export default function SingleProduct() {
         <div className="w-10/12 md:w-1/2 flex flex-col gap-5">
             <h2 className="text-5xl font-bold text-blue-800">Anillo</h2>
             <p className="text-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, officia quis ratione culpa dolore cupiditate, praesentium incidunt ab aperiam, asperiores exercitationem rerum illum repellat dolores ex nostrum veniam labore obcaecati.</p>
-            <p className="text-2xl text-purplemain font-medium">$100000</p>
+            <p className="text-4xl mb-5 text-purplemain font-medium">$100000</p>
             <div className="">
               <NavLink to="/" className="bg-blue-800 mr-2 mt-5 md:mt-10 rounded-full border w-fit px-5 py-2 text-white text-xl transition hover:bg-white hover:border-blue-800 hover:text-blue-800">Comprar</NavLink>
               <NavLink to="/" className="border border-blue-800 mt-5 md:mt-10 rounded-full w-fit px-5 py-2 text-blue-800 text-xl hover:bg-blue-800 hover:text-white transition">Añadir al carrito</NavLink>
@@ -24,7 +24,7 @@ export default function SingleProduct() {
         </div>
       </div>
       <hr className="border-2 m-auto border-purplemain w-32" />
-      <div className="w-full lg:w-4/6 m-auto my-5 px-5" style={{fontFamily: 'quador'}}>
+      <div className="w-full lg:w-4/6 m-auto my-5 px-1" style={{fontFamily: 'quador'}}>
         <h2 className="text-center text-blue-800 font-semibold text-3xl mb-2">Productos relacionados</h2>
         <div className="w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-1">
           <Card />
